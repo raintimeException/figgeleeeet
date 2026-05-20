5dfac11555542e9684da73b07257c6c449cbbeea
Ivan Kovalenko <raintimeUnhandledEXcepti0n@gmail.com>
2026-05-20 20:49:00 +0200

    refactor, +B

--- main.c
+++ main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
 #define FONT_SZ 4
 #define LETTER_WIDTH 6
 #define TOTAL_LETTERS 128
 
+// TODO: more
 char A[FONT_SZ][LETTER_WIDTH] = {
     "  __  ",
     " /  \\ ",
@@ -13,30 +15,47 @@ char A[FONT_SZ][LETTER_WIDTH] = {
     "|    |",
 };
 
-char C[FONT_SZ][LETTER_WIDTH] = {
-    " ____",
+char B[FONT_SZ][LETTER_WIDTH] = {
+    ".----.",
     "|    |",
-    "|    ",
+    "|----|",
     "|____|",
 };
 
+char C[FONT_SZ][LETTER_WIDTH] = {
+    " ____ ",
+    "|    |",
+    "|     ",
+    "|____|",
+};
+
+char *glyph[TOTAL_LETTERS][FONT_SZ];
 
 int main(int argc, char *argv[])
 {
 
-    (void)argc;
-    (void)argv;
-    char *chars = "AC";
-    int chars_count = strlen(chars);
-    char *glyph[TOTAL_LETTERS][FONT_SZ];
-
-    for (int i = 0; i < FONT_SZ; ++i) {
-        glyph['A'][i] = malloc(LETTER_WIDTH*sizeof(char));
-        memcpy(glyph['A'][i], A[i], LETTER_WIDTH);
+    const char *supported_chars = "ABC";
+    char *prog_name = *argv++;
+    if (argc < 2) {
+        printf("usage: ");
+        printf("%s ", prog_name);
+        printf("[%s]\n", supported_chars);
+        return -1;
     }
-    for (int i = 0; i < FONT_SZ; ++i) {
-        glyph['C'][i] = malloc(LETTER_WIDTH*sizeof(char));
-        memcpy(glyph['C'][i], C[i], LETTER_WIDTH);
+
+    char *chars = *argv;
+    int chars_count = strlen(chars);
+    for (int c = 0; c < chars_count; ++c) {
+        for (int i = 0; i < FONT_SZ; ++i) {
+            char curr_char = chars[c];
+            glyph[curr_char][i] = malloc(LETTER_WIDTH*sizeof(char));
+            // fixme
+            switch (curr_char) {
+                case 'A': memcpy(glyph[curr_char][i], A[i], LETTER_WIDTH); break;
+                case 'B': memcpy(glyph[curr_char][i], B[i], LETTER_WIDTH); break;
+                case 'C': memcpy(glyph[curr_char][i], C[i], LETTER_WIDTH); break;
+            }
+        }
     }
     char *out[FONT_SZ];
 

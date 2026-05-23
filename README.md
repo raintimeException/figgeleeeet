4260058d77d186491bbd3516984c215278d45d19
Ivan Kovalenko <raintimeUnhandledEXcepti0n@gmail.com>
2026-05-23 15:23:46 +0200

    huge rewrite...

---
+++ README.md
@@ -0,0 +1,5 @@
+# figgeleeeet
+
+<p align=center>
+  <img src="./resources/img/example.png">
+</p>
--- main.c
+++ main.c
@@ -4,217 +4,257 @@
 #include <assert.h>
 
 #define FONT_SZ 5
-#define LETTER_WIDTH 6
+#define LETTER_WIDTH 7
 #define TOTAL_LETTERS 128
 
-char A[FONT_SZ][LETTER_WIDTH] = {
+#define ARRSZ(x) (sizeof(x)/sizeof(*x))
+
+#define IS_LOWER(c) ((c) >= 'a' && (c) <= 'z')
+#define IS_UPPER(c) ((c) >= 'A' && (c) <= 'Z')
+
+#define TO_UPPER_CASE(c) (((c) >= 'a' && (c) <= 'z') ? c -= 32 : c)
+#define TO_LOWER_CASE(c) (((c) >= 'A' && (c) <= 'Z') ? c += 32 : c)
+
+typedef enum {false, true}bool;
+
+bool _is_space(char c)
+{
+    return c == ' ' || c == '\t' || c == '\n';
+}
+
+
+typedef struct {
+    char c;
+    char data[FONT_SZ][LETTER_WIDTH];
+} Font;
+
+Font *get_font_from_font_arr_with_c(Font *font_arr, int font_arr_len, char c)
+{
+    assert(font_arr != NULL);
+    assert(font_arr_len >= 0);
+    assert(c >= 'A' && c <= 'Z' || _is_space(c));
+
+    for (int pos = 0; pos < font_arr_len; ++pos) {
+        if (font_arr[pos].c == c)
+            return &font_arr[pos];
+    }
+    return NULL;
+}
+
+void _null_terminate_font_data_row(Font *font, int row)
+{
+    assert(font != NULL);
+    assert(row <= FONT_SZ);
+
+    font->data[row][LETTER_WIDTH-1] = 0;
+    assert(font->data[row][LETTER_WIDTH-1] == 0);
+}
+
+char *get_data_row_from_font(Font *font_arr, int font_arr_len, int row, char c) {
+    assert(font_arr != NULL);
+    assert(font_arr_len >= 0);
+    assert(row <= FONT_SZ);
+    assert(c >= 'A' && c <= 'Z' || _is_space(c));
+
+    Font *_font = get_font_from_font_arr_with_c(font_arr, font_arr_len, c);
+    assert(_font != NULL);
+
+    _null_terminate_font_data_row(_font, row);
+    return _font->data[row];
+}
+
+
+Font font_arr[] = {
+    {' ', {
+    "      ",
+    "      ",
+    "      ",
+    "      ",
+    "      ",
+}},
+    {'A', {
     "  __  ",
     " /  \\ ",
     "|    |",
     "|----|",
     "|    |",
-};
-
-char B[FONT_SZ][LETTER_WIDTH] = {
-    " ____ ",
-    "|    |",
-    "|----|",
-    "|    |",
-    "|____|",
-};
-
-char C[FONT_SZ][LETTER_WIDTH] = {
-    " ____ ",
-    "|    |",
-    "|     ",
-    "|    |",
-    "|____|",
-};
-
-char D[FONT_SZ][LETTER_WIDTH] = {
-    " ____ ",
-    "|    \\",
-    "|    |",
-    "|    |",
-    "\\____|",
-};
-
-char E[FONT_SZ][LETTER_WIDTH] = {
-    " _____",
-    "|     ",
-    "|-----",
-    "|     ",
-    "|_____",
-};
-
-char F[FONT_SZ][LETTER_WIDTH] = {
-    " _____",
-    "|     ",
-    "|-----",
-    "|     ",
-    "|     ",
-};
-
-char G[FONT_SZ][LETTER_WIDTH] = {
-    " ____ ",
-    "/    \\",
-    "|   _ ",
+}},
+    {'B', {
+    ".___. ",
+    "|   | ",
+    "|---\\ ",
     "|    |",
     "|___/ ",
-};
-
-char H[FONT_SZ][LETTER_WIDTH] = {
-    "      ",
+}},
+    {'C', {
+    ".____.",
+    "|    |",
+    "|     ",
+    "|    .",
+    "|____|",
+}},
+    {'D', {
+    ".__.  ",
+    "|   \\ ",
+    "|    \\",
+    "|    /",
+    "|___/ ",
+}},
+    {'E', {
+    ".____.",
+    "|     ",
+    "|---- ",
+    "|     ",
+    "|____.",
+}},
+    {'F', {
+    ".____.",
+    "|     ",
+    "|---- ",
+    "|     ",
+    "|     ",
+}},
+    {'G', {
+    " .__. ",
+    "/    \\",
+    "|   _.",
+    "|    /",
+    "|___/ ",
+}},
+    {'H', {
+    ".    .",
+    "|    |",
+    "|____|",
     "|    |",
     "|    |",
-    "|----|",
-    "|    |",
-};
-
-char I[FONT_SZ][LETTER_WIDTH] = {
-    "      ",
+}},
+    {'I', {
+    "   .  ",
     "   |  ",
     "   |  ",
     "   |  ",
     "   |  ",
-};
-
-char J[FONT_SZ][LETTER_WIDTH] = {
-    "      ",
+}},
+    {'J', {
+    "   .  ",
     "   |  ",
-    "   |  ",
-    "   /  ",
-    "\\_/   ",
-};
-
-char K[FONT_SZ][LETTER_WIDTH] = {
-    "      ",
+    ".  |  ",
+    "|  |  ",
+    " \\/   ",
+}},
+    {'K', {
+    ".    .",
     "|   / ",
     "|__/  ",
     "|  \\  ",
     "|   \\ ",
-};
-
-char L[FONT_SZ][LETTER_WIDTH] = {
-    "      ",
+}},
+    {'L', {
+    ".     ",
     "|     ",
     "|     ",
     "|     ",
-    "\\_____",
-};
-
-char M[FONT_SZ][LETTER_WIDTH] = {
-    "      ",
-    "|\\  | ",
-    "| \\/| ",
-    "|   | ",
-    "|   | ",
-};
-
-char N[FONT_SZ][LETTER_WIDTH] = {
-    "      ",
+    "|____ ",
+}},
+    {'M', {
+    ".    .",
+    "|    |",
+    "|\\__/|",
+    "|    |",
+    "|    |",
+}},
+    {'N', {
+    ".   . ",
     "|\\  | ",
     "| \\ | ",
     "|  \\| ",
     "|   | ",
-};
-
-char O[FONT_SZ][LETTER_WIDTH] = {
-    " ____ ",
+}},
+    {'O', {
+    " ____.",
     "/    |",
     "|    |",
     "|    |",
     "|____|",
-};
-
-char P[FONT_SZ][LETTER_WIDTH] = {
+}},
+    {'P', {
     " ____ ",
     "|    |",
     "|____|",
     "|     ",
     "|     ",
-};
-
-char Q[FONT_SZ][LETTER_WIDTH] = {
+}},
+    {'Q', {
     " ____ ",
     "/   | ",
     "|   | ",
     "|   | ",
     "|__/\\ ",
-};
-
-char R[FONT_SZ][LETTER_WIDTH] = {
-    " ____ ",
-    "/    |",
+}},
+    {'R', {
+    ".____.",
     "|    |",
-    "|___/ ",
-    "|   | ",
-};
-
-char S[FONT_SZ][LETTER_WIDTH] = {
-    " ____",
+    "|----*",
+    "|\\___.",
+    "|    |",
+}},
+    {'S', {
+    ".___.",
     "|    ",
-    "|____",
+    "|___.",
     "    |",
-    "____|",
-};
-
-char T[FONT_SZ][LETTER_WIDTH] = {
-    "_____ ",
+    ".___|",
+}},
+    {'T', {
+    ".____.",
     "  |   ",
     "  |   ",
     "  |   ",
     "  |   ",
-};
-
-char U[FONT_SZ][LETTER_WIDTH] = {
-    "      ",
+}},
+    {'U', {
+    ".   . ",
     "|   | ",
     "|   | ",
     "|  /| ",
     "|_/ |_",
-};
-
-char V[FONT_SZ][LETTER_WIDTH] = {
-    "      ",
+}},
+    {'V', {
+    ".    .",
     "|    |",
     "|    |",
     "|    |",
-    "|___/ ",
-};
-
-char W[FONT_SZ][LETTER_WIDTH] = {
-    "      ",
-    "| |  |",
-    "| |  |",
-    "| |  |",
-    "|_|__|",
-};
-
-char X[FONT_SZ][LETTER_WIDTH] = {
-    " ___",
+    " \\__/ ",
+}},
+    {'W', {
+    ". . . ",
+    "| | | ",
+    "| | | ",
+    "| | | ",
+    " \\|/  ",
+}},
+    {'X', {
+    ".   .",
+    " \\|/ ",
+    "  |  ",
+    " /|\\ ",
     "| | |",
-    "  |  ",
-    "  |  ",
-    "|_|_|",
-};
-
-char Y[FONT_SZ][LETTER_WIDTH] = {
-    "      ",
+}},
+    {'Y', {
+    ".    .",
     "|    |",
     "|____|",
     "     |",
-    "  __/ ",
-};
-
-char Z[FONT_SZ][LETTER_WIDTH] = {
-    "____  ",
+    " .__/ ",
+}},
+    {'Z', {
+    ".___. ",
     "   /  ",
     "  /   ",
     " /    ",
-    "/____",
+    "/___. ",
+}},
 };
-
 
 
 char *glyph[TOTAL_LETTERS][FONT_SZ];
@@ -224,7 +264,6 @@ int main(int argc, char *argv[])
     const char *supported_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     char *prog_name = *argv++;
     if (argc < 2) {
-usage:
         printf("usage: ");
         printf("%s ", prog_name);
         printf("[%s]\n", supported_chars);
@@ -232,67 +271,34 @@ usage:
     }
     char *chars = *argv;
     int chars_count = strlen(chars);
-    for (int c = 0; c < chars_count; ++c) {
-        for (int i = 0; i < FONT_SZ; ++i) {
-            char curr_char = chars[c];
-            glyph[curr_char][i] = malloc(LETTER_WIDTH*sizeof(char));
-            // FIXME
-            switch (curr_char) {
-                case 'A': memcpy(glyph[curr_char][i], A[i], LETTER_WIDTH); break;
-                case 'B': memcpy(glyph[curr_char][i], B[i], LETTER_WIDTH); break;
-                case 'C': memcpy(glyph[curr_char][i], C[i], LETTER_WIDTH); break;
-                case 'D': memcpy(glyph[curr_char][i], D[i], LETTER_WIDTH); break;
-                case 'E': memcpy(glyph[curr_char][i], E[i], LETTER_WIDTH); break;
-                case 'F': memcpy(glyph[curr_char][i], F[i], LETTER_WIDTH); break;
-                case 'G': memcpy(glyph[curr_char][i], G[i], LETTER_WIDTH); break;
-                case 'H': memcpy(glyph[curr_char][i], H[i], LETTER_WIDTH); break;
-                case 'I': memcpy(glyph[curr_char][i], I[i], LETTER_WIDTH); break;
-                case 'J': memcpy(glyph[curr_char][i], J[i], LETTER_WIDTH); break;
-                case 'K': memcpy(glyph[curr_char][i], K[i], LETTER_WIDTH); break;
-                case 'L': memcpy(glyph[curr_char][i], L[i], LETTER_WIDTH); break;
-                case 'M': memcpy(glyph[curr_char][i], M[i], LETTER_WIDTH); break;
-                case 'N': memcpy(glyph[curr_char][i], N[i], LETTER_WIDTH); break;
-                case 'O': memcpy(glyph[curr_char][i], O[i], LETTER_WIDTH); break;
-                case 'P': memcpy(glyph[curr_char][i], P[i], LETTER_WIDTH); break;
-                case 'Q': memcpy(glyph[curr_char][i], Q[i], LETTER_WIDTH); break;
-                case 'R': memcpy(glyph[curr_char][i], R[i], LETTER_WIDTH); break;
-                case 'S': memcpy(glyph[curr_char][i], S[i], LETTER_WIDTH); break;
-                case 'T': memcpy(glyph[curr_char][i], T[i], LETTER_WIDTH); break;
-                case 'U': memcpy(glyph[curr_char][i], U[i], LETTER_WIDTH); break;
-                case 'V': memcpy(glyph[curr_char][i], V[i], LETTER_WIDTH); break;
-                case 'W': memcpy(glyph[curr_char][i], W[i], LETTER_WIDTH); break;
-                case 'X': memcpy(glyph[curr_char][i], X[i], LETTER_WIDTH); break;
-                case 'Y': memcpy(glyph[curr_char][i], Y[i], LETTER_WIDTH); break;
-                case 'Z': memcpy(glyph[curr_char][i], Z[i], LETTER_WIDTH); break;
-                default:
-                    goto usage;
-            }
-        }
-    }
-    char *out[FONT_SZ];
 
-    int line_len = LETTER_WIDTH * chars_count * sizeof(char);
-    int is_letter_num = 1;
-    for (int i = 0; i < FONT_SZ; ++i) {
-        char *line = malloc(line_len);
-        int k;
-        for (int j = 0; j < chars_count; ++j) {
-            strcat(line, glyph[chars[j]][i]);
+    char *out[FONT_SZ];
+    char *line;
+    int line_len = LETTER_WIDTH * chars_count * sizeof(char) + 1;
+
+    for (int row = 0; row < FONT_SZ; ++row) {
+        out[row] = malloc(line_len*sizeof(char) + 1);
+        assert(out[row] != NULL);
+        line = malloc(line_len*sizeof(char) + 1);
+        assert(line != NULL);
+
+        for (int ci = 0; ci < chars_count; ++ci) {
+            char c = TO_UPPER_CASE(chars[ci]);
+            char *data = get_data_row_from_font(font_arr, ARRSZ(font_arr), row, c);
+            strcat(line, data);
         }
-        line[line_len] = 0;
-        out[i] = malloc(line_len + 1);
-        memcpy(out[i], line, line_len + 1);
+        memcpy(out[row], line, line_len - 1);
     }
+
     for (int i = 0; i < FONT_SZ; ++i) {
         printf("%s\n", out[i]);
     }
-
     return 0;
 }
 
 /*
  * TODO:
- * no libs, only standard...
- * no malloc, no freee...
- *
+ * [] give the possibility choose the size of the font x2, x3
+ * [] no libs, only standard... (unistd.h)
+ * [] no heap allocation
  */
